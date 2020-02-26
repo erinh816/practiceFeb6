b0fcd7ee4e2081e76279bb1b6fa1757293cb5194
Silu Hu <erinh816@gmail.com>
2020-02-25 22:34:45 -0500

    finish 76

--- note-app/notes-app.js
+++ note-app/notes-app.js
@@ -105,3 +105,46 @@ document.querySelector('#filter-by').addEventListener('change', function (event)
     console.log(event.target.value)
 })
 
+
+
+//moment basically a function
+
+const now = moment()
+console.log(now.toString())
+
+// now.minute(1)
+// console.log(now.minute())
+
+now.add(1, 'year').subtract(20, 'days')
+console.log(now.format())
+
+//November 3rd, 2003
+console.log(now.format('MMMM Do, YYYY'))
+
+console.log(now.fromNow())
+
+const nowTimestamp = now.valueOf()
+console.log(nowTimestamp)
+
+console.log(moment(nowTimestamp).toString())
+
+
+//1. Create a new moment
+//2. Set the month, day, and year to your birthday
+//3. Use format to print it in the following way:Jan 6, 1991
+
+const newTime = moment()
+console.log(newTime)
+
+newTime.year(1990).month(7).date(16)
+console.log(newTime.toString())
+
+// newTime.subtract(30, 'year').subtract(9, 'day').add(6, 'month')
+// console.log(newTime.toString())
+
+console.log(newTime.format('MMM D, YYYY'))
+
+
+
+
+
--- note-app/notes-edit.js
+++ note-app/notes-edit.js
@@ -6,7 +6,7 @@ const noteId = location.hash.substring(1); //from first index to the end
 
 let notes = getSavedNotes();
 
-let note = notes.find(function(note) {
+let note = notes.find(function (note) {
 	return note.id === noteId;
 });
 
@@ -22,27 +22,27 @@ bodyElement.value = note.body;
 //3. Repeat steps 1-2 for body
 //4. Setup a remove button that removes notes and sends user back to home page
 
-titleElement.addEventListener('input', function(event) {
+titleElement.addEventListener('input', function (event) {
 	note.title = event.target.value;
 	saveNotes(notes);
 });
 
-bodyElement.addEventListener('input', function(event) {
+bodyElement.addEventListener('input', function (event) {
 	note.body = event.target.value;
 	saveNotes(notes);
 });
 
-removeElement.addEventListener('click', function(event) {
+removeElement.addEventListener('click', function (event) {
 	removeNote(note.id);
 	saveNotes(notes);
 	location.assign('index.html');
 });
 
-window.addEventListener('storage', function(event) {
+window.addEventListener('storage', function (event) {
 	if (event.key === 'notes') {
 		JSON.parse(event.newValue);
 
-		note = notes.find(function(note) {
+		note = notes.find(function (note) {
 			return note.id === noteId;
 		});
 
@@ -56,59 +56,15 @@ window.addEventListener('storage', function(event) {
 });
 
 //sync homepage as well
-window.addEventListener('storage',function(event){
-    if(event.key === 'notes'){
-        //parse the new data and update notes
-        //rerender the notes
-        notes = JSON.parse(event.newValue)
-        //my understanding:update notes with newValue typed
-        renderNotes(notes, filters)
-        //my understanding:rerender
-    }
+window.addEventListener('storage', function (event) {
+	if (event.key === 'notes') {
+		//parse the new data and update notes
+		//rerender the notes
+		notes = JSON.parse(event.newValue)
+		//my understanding:update notes with newValue typed
+		renderNotes(notes, filters)
+		//my understanding:rerender
+	}
 })
 
 
-//create the current time
-// const now = new Date()
-// console.log(now)
-
-// console.log(`Year:${now.getFullYear()}`)
-// console.log(`Month:${now.getMonth()}`)
-// console.log(`Day:${now.getDate()}`)
-// console.log(`Hour:${now.getHours()}`)
-// console.log(`Minute:${now.getMinutes()}`)
-// console.log(`Second:${now.getSeconds()}`)
-
-//create date at a different point of time
-// const then = new Date('January 21 2001 6:25:01')
-// console.log(then)
-// console.log(typeof(then))
-
-//*
-//we typically don't do line 83 defining a specific time
-//Insdead, we Unix Epoch - January 1st 1970 00:00:00
-//time after this +(positive), before this - (negative)
-//0 represents this time exactly
-//1000 is 1 second after it (unit:millesecond)
-//-60000 is a minute before it
-
-const now = new Date()
-const timestamp = now.getTime() //big number
-
-
-const myDate = new Date(timestamp) //Mon Feb 24
-console.log(myDate.getFullYear()) //2020
-
-const date1 = new Date('January 14 1995 6:35:01')
-const date2 = new Date('March 30 2009 19:20:06')
-
-const timestamp1 = date1.getTime()
-const timestamp2 = date2.getTime()
-
-
-
-if(timestamp1<timestamp2){
-    console.log(`${date1} is first`)
-}else{
-    console.log(`${date2} is first`)
-}
