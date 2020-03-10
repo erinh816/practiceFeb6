5271544510cdbccb23483fecc8d7e74d6c4632c4
erin <erinh816@gmail.com>
2020-03-09 23:00:57 -0400

    finish 91

--- oop/person.js
+++ oop/person.js
@@ -1,14 +1,35 @@
-const Person = function (firstName, lastName, age) {
-    // console.log(this) //this is the empty person {} object
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-}
+//Prototypal Inheritance
 
-const me = new Person('Andrew', 'Mead', 27)
-me.firstName = 'Jennifer'
-console.log(me)
-console.log(me.age)
+const Person = function(firstName, lastName, age, likes = []) {
+	// console.log(this) //this is the empty person {} object
+	// set the default likes to an empty array
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.age = age;
+	this.likes = likes;
+};
 
-const person2 = new Person('Amy', 'Lang', 30)
-console.log(person2)
+Person.prototype.getBio = function() {
+	let bio = `${this.firstName} is ${this.age}`;
+	this.likes.forEach((like) => {
+		bio = bio + `${this.firstName} likes ${like}`;
+	});
+	return bio;
+};
+
+Person.prototype.setName = function(fullName) {
+	const names = fullName.split(' ');
+	this.firstName = names[0];
+	this.lastName = names[1];
+};
+
+const me = new Person('Andrew', 'Mead', 27, [ 'Teaching', 'Biking' ]);
+me.firstName = 'Jennifer';
+me.setName('Alexis Ren');
+console.log(me);
+console.log(me.age);
+console.log(me.getBio());
+
+const person2 = new Person('Amy', 'Lang', 30);
+console.log(person2);
+console.log(person2.getBio());
